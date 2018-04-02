a339b35e1490b8d95ef3b90730bca6d8e7297bc6
gugug <734093894@qq.com>
2018-04-02 21:52:15 +0800

    订单表

--- src/main/java/com/gdufs/bookstore/model/Order.java
+++ src/main/java/com/gdufs/bookstore/model/Order.java
@@ -6,10 +6,10 @@ import lombok.Data;
  * Created by gu on 2018/4/2.
  */
 @Data
-public class ShoppingCart {
-    private long cartid;
+public class Order {
+    private long orderid;
     private long userid;
     private long bookid;
-    private int num;
-    private int orderState;
+    private java.sql.Timestamp ordertime;
+    private int shippingState;
 }
