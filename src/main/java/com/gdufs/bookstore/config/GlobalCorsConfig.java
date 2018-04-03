a7aae9980e04af8cd5b0d996d0ea5ac7a1c6f958
gugug <734093894@qq.com>
2018-04-03 16:35:37 +0800

    跨域問題

---
+++ src/main/java/com/gdufs/bookstore/config/GlobalCorsConfig.java
@@ -0,0 +1,27 @@
+package com.gdufs.bookstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 跨域
+ */
+@Configuration
+public class GlobalCorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
+
