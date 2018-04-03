64a2f4f4a3f3885d1f313d871a000986ebdab0d4
gugug <734093894@qq.com>
2018-04-03 15:01:56 +0800

    config

--- src/main/java/com/gdufs/bookstore/BookstoreApplication.java
+++ src/main/java/com/gdufs/bookstore/BookstoreApplication.java
@@ -1,9 +1,11 @@
 package com.gdufs.bookstore;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.gdufs.bookstore.dao")
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
